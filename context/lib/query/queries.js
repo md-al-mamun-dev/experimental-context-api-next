f0c5d41ee63039a11d54f86877b8b31d5d20f17c
MD.AL MAMUN <mamunofficialmail@gmail.com>
2024-11-13 18:16:59 +0600

    some changes

--- context/lib/query/queries.js
+++ context/lib/query/queries.js
@@ -4,29 +4,15 @@ import usePUT from './queryHooks/usePUT';
 import usePATCH from './queryHooks/usePATCH';
 import useDELETE from './queryHooks/useDELETE';
 
-export const queries = ({baseQuery, useData, useDispatch}) => ({
-    get: (config) => (params) => {
-        console.log(params)
-        const cacheName = params;
-        const url = config['query']()
-        return useGET(cacheName, baseQuery + url, useData, useDispatch);
-    },
-    post: (config) => (params) => {
-        const cacheName = config.fnName;
-        return usePOST(cacheName, baseQuery + url, useData, useDispatch);
-    },
-    put: (config) => (params) => {
-        const cacheName = config.fnName;
-        return usePUT(cacheName, baseQuery + config.url, params);
-    },
-    patch: (config) => (params) => {
-        const cacheName = config.fnName;
-        return usePATCH(cacheName, baseQuery + config.url, params);
-    },
-    delete: (config) => (params) => {
-        const cacheName = config.fnName;
-        return useDELETE(cacheName, baseQuery + config.url, params);
-    },
-});
+export const queries = {
+    get:({ query})=>({fnName, params, baseQuery, useData, useDispatch})=>{ 
+        const url = baseQuery+ query(params)
+       return  useGET(fnName, params, url, useData, useDispatch )
+    } ,
+    // post:()=> {},
+    // put:()=> {},
+    // patch:()=> {},
+    // delete:()=> {},
+};
 
 export default  queries  
--- context/lib/query/queryHooks/useGET.js
+++ context/lib/query/queryHooks/useGET.js
@@ -1,33 +1,34 @@
-import useDispatch from "../../layout/useDispatch";
-import useData from "../../layout/useData";
-import { dataCache } from "../../__createApiSlice";
+
 import { useEffect } from "react";
 
-export default function useGET(cacheName, url) {
-    const data = useData();
-    const dispatch = useDispatch();
+export default function useGET(fnName, params, url, useData, useDispatch) {
 
-    useEffect(() => {
-        console.log(cacheName)
-        const fetchData = async () => {
-            dispatch({ type: `${cacheName}/LOADING`, endpoint: cacheName });
-            try {
-                const cachedData = await dataCache.getData(cacheName, url, '');
-                dispatch({ type: `${cacheName}/SUCCESS`, payload: cachedData, endpoint: cacheName });
-            } catch (error) {
-                dispatch({ type: `${cacheName}/ERROR`, payload: error.message, endpoint: cacheName });
-            }
-        };
+    const data = useData()
 
-        fetchData();
 
-        const handleDataUpdate = ({ key }) => {
-            if (key === dataCache.generateCacheKey(cacheName, '')) fetchData();
-        };
+    return { data }
 
-        dataCache.on('dataUpdate', handleDataUpdate);
-        return () => dataCache.off('dataUpdate', handleDataUpdate);
-    }, [cacheName, url]);
+    // useEffect(() => {
+    //     console.log(cacheName)
+    //     const fetchData = async () => {
+    //         dispatch({ type: `${cacheName}/LOADING`, endpoint: cacheName });
+    //         try {
+    //             const cachedData = await dataCache.getData(cacheName, url, '');
+    //             dispatch({ type: `${cacheName}/SUCCESS`, payload: cachedData, endpoint: cacheName });
+    //         } catch (error) {
+    //             dispatch({ type: `${cacheName}/ERROR`, payload: error.message, endpoint: cacheName });
+    //         }
+    //     };
 
-    return { data: data?.[cacheName]?.data || {}, loading: data?.[cacheName]?.loading, error: data?.[cacheName]?.error };
+    //     fetchData();
+
+    //     const handleDataUpdate = ({ key }) => {
+    //         if (key === dataCache.generateCacheKey(cacheName, '')) fetchData();
+    //     };
+
+    //     dataCache.on('dataUpdate', handleDataUpdate);
+    //     return () => dataCache.off('dataUpdate', handleDataUpdate);
+    // }, [cacheName, url]);
+
+    // return { data: data?.[cacheName]?.data || {}, loading: data?.[cacheName]?.loading, error: data?.[cacheName]?.error };
 }
